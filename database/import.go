0b749928a0da2482c2867a9021ec83118b191914
desss4116 <aminjon01q@gmail.com>
2026-05-07 18:10:58 +0500

    Update import.go

--- database/import.go
+++ database/import.go
@@ -1,1 +1,21 @@
+package database
 
+func AddKnowledge(
+	category string,
+	title string,
+	content string,
+) {
+
+	DB.Exec(`
+	INSERT INTO knowledge_base(
+	category,
+	title,
+	content
+	)
+	VALUES(?,?,?)
+	`,
+		category,
+		title,
+		content,
+	)
+}
