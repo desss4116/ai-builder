ee63c2563b863fbe54e35fdbc7f252655d5c204a
desss4116 <aminjon01q@gmail.com>
2026-05-07 17:49:32 +0500

    Update knowledge.go

--- engine/knowledge.go
+++ engine/knowledge.go
@@ -1,1 +1,23 @@
+package engine
 
+type BusinessKnowledge struct {
+	Sections []string
+}
+
+var EcommerceKnowledge = BusinessKnowledge{
+	Sections: []string{
+		"hero",
+		"products",
+		"reviews",
+		"cta",
+	},
+}
+
+var RestaurantKnowledge = BusinessKnowledge{
+	Sections: []string{
+		"hero",
+		"menu",
+		"gallery",
+		"reviews",
+	},
+}
