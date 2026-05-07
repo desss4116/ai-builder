ae4aca1f4c1734fa7535dc166ba1b6e06f604a74
desss4116 <aminjon01q@gmail.com>
2026-05-07 21:27:00 +0500

    Create testimonials.go

---
+++ components/testimonials.go
@@ -0,0 +1,30 @@
+package components
+
+func Testimonials() string {
+
+	return `
+<section>
+
+<div class="container">
+
+<h2>
+Testimonials
+</h2>
+
+<div class="grid">
+
+<div class="card testimonial fade-up">
+"This platform completely changed our business."
+</div>
+
+<div class="card testimonial fade-up">
+"Looks better than expensive agencies."
+</div>
+
+</div>
+
+</div>
+
+</section>
+`
+}
