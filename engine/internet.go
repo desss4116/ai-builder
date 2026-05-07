85e806ad2d0aa3764b306c79cb0f4e9928e4318d
desss4116 <aminjon01q@gmail.com>
2026-05-07 17:50:19 +0500

    Update internet.go

--- engine/internet.go
+++ engine/internet.go
@@ -1,1 +1,21 @@
+package engine
 
+import (
+	"io"
+	"net/http"
+)
+
+func FetchWebsite(url string) string {
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return ""
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+
+	return string(body)
+}
