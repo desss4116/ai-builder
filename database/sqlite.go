fe1fe3764d4709a50d3a19377bcd592d0a41582d
desss4116 <aminjon01q@gmail.com>
2026-05-07 22:10:09 +0500

    Update sqlite.go

--- database/sqlite.go
+++ database/sqlite.go
@@ -2,66 +2,37 @@ package database
 
 import (
 	"database/sql"
+	_ "://github.com"
 	"log"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-
 	var err error
-
-	DB, err = sql.Open(
-		"sqlite3",
-		"knowledge.db",
-	)
-
+	DB, err = sql.Open("sqlite3", "./ai_builder.db")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	optimizeSQLite()
-
-	createTables()
-}
-
-func optimizeSQLite() {
-
-	pragmas := []string{
-
-		"PRAGMA journal_mode=WAL;",
-
-		"PRAGMA synchronous=OFF;",
-
-		"PRAGMA temp_store=MEMORY;",
-
-		"PRAGMA cache_size=-20000;",
-
-		"PRAGMA mmap_size=30000000000;",
-	}
-
-	for _, p := range pragmas {
-
-		DB.Exec(p)
-	}
-}
-
-func createTables() {
-
+	// Таблица для "знаний" системы (Knowledge Base)
 	query := `
-	CREATE VIRTUAL TABLE IF NOT EXISTS knowledge_base
-	USING fts5(
-		category,
-		title,
-		content
+	CREATE TABLE IF NOT EXISTS knowledge (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		tag TEXT,
+		content TEXT,
+		source TEXT
 	);
-	`
-
-	_, err := DB.Exec(query)
-
+	CREATE TABLE IF NOT EXISTS sites (
+		id TEXT PRIMARY KEY,
+		prompt TEXT,
+		html TEXT,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);`
+	
+	_, err = DB.Exec(query)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
