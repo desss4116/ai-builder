b7e4087132de10a6fea2e56c4672cad23adc9e6f
desss4116 <aminjon01q@gmail.com>
2026-05-07 21:26:22 +0500

    Create products.go

---
+++ components/products.go
@@ -0,0 +1,54 @@
+package components
+
+func Products() string {
+
+	return `
+<section>
+
+<div class="container">
+
+<h2>
+Products
+</h2>
+
+<div class="grid">
+
+<div class="card product-card fade-up">
+
+<img
+src="https://images.unsplash.com/photo-1542291026-7eec264c27ff"
+>
+
+<h3>
+Premium Sneakers
+</h3>
+
+<p>
+Modern ecommerce experience.
+</p>
+
+</div>
+
+<div class="card product-card fade-up">
+
+<img
+src="https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77"
+>
+
+<h3>
+Luxury Collection
+</h3>
+
+<p>
+Responsive modern design.
+</p>
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+`
+}
