34367a143547c8f258713e1cff8cf7f99f5d8b82
desss4116 <aminjon01q@gmail.com>
2026-05-07 18:10:06 +0500

    Update search.go

--- database/search.go
+++ database/search.go
@@ -1,1 +1,43 @@
+package database
 
+type SearchResult struct {
+	Title   string
+	Content string
+}
+
+func SearchKnowledge(
+	query string,
+) []SearchResult {
+
+	rows, err := DB.Query(`
+	SELECT
+	title,
+	content
+	FROM knowledge_base
+	WHERE knowledge_base MATCH ?
+	ORDER BY bm25(knowledge_base)
+	LIMIT 5
+	`, query)
+
+	if err != nil {
+		return nil
+	}
+
+	defer rows.Close()
+
+	results := []SearchResult{}
+
+	for rows.Next() {
+
+		var r SearchResult
+
+		rows.Scan(
+			&r.Title,
+			&r.Content,
+		)
+
+		results = append(results, r)
+	}
+
+	return results
+}
