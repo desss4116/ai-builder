1aa74a0ea3d6413a349b80af130af30fd72bd39b
desss4116 <aminjon01q@gmail.com>
2026-05-08 18:40:00 +0500

    Update search.go

--- internet/search.go
+++ internet/search.go
@@ -1,18 +1,67 @@
 package internet
 
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"time"
+)
+
 type SearchResult struct {
 	Title string
 	URL   string
 }
 
+type DuckResponse struct {
+	RelatedTopics []struct {
+		Text     string `json:"Text"`
+		FirstURL string `json:"FirstURL"`
+	} `json:"RelatedTopics"`
+}
+
 func Search(query string) []SearchResult {
 
-	results := []SearchResult{
+	endpoint :=
+		"https://api.duckduckgo.com/?q=" +
+			url.QueryEscape(query) +
+			"&format=json&no_html=1&skip_disambig=1"
 
-		{
-			Title: query,
-			URL:   "https://en.wikipedia.org/wiki/" + query,
-		},
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	resp, err := client.Get(endpoint)
+
+	if err != nil {
+		return nil
+	}
+
+	defer resp.Body.Close()
+
+	var data DuckResponse
+
+	err = json.NewDecoder(resp.Body).Decode(&data)
+
+	if err != nil {
+		return nil
+	}
+
+	var results []SearchResult
+
+	for _, item := range data.RelatedTopics {
+
+		if item.FirstURL == "" {
+			continue
+		}
+
+		results = append(results, SearchResult{
+			Title: item.Text,
+			URL:   item.FirstURL,
+		})
+
+		if len(results) >= 5 {
+			break
+		}
 	}
 
 	return results
