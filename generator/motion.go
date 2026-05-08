0397d71b925aef337d2086b523b87a1373c1eaa2
desss4116 <aminjon01q@gmail.com>
2026-05-08 17:12:19 +0500

    Create motion.go

---
+++ generator/motion.go
@@ -0,0 +1,14 @@
+package generator
+
+func GenerateMotion() string {
+
+	return `
+Motion System:
+
+- stagger animations
+- smooth transitions
+- hover interactions
+- cinematic scrolling
+- premium easing
+`
+}
