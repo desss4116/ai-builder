409f97ae14594718b34d5e057ca2730b4d3e9137
desss4116 <aminjon01q@gmail.com>
2026-05-08 15:15:53 +0500

    Update sites.go

--- storage/sites.go
+++ storage/sites.go
@@ -1,1 +1,30 @@
+package storage
 
+import (
+	"encoding/json"
+	"os"
+)
+
+type Site struct {
+	ID    string `json:"id"`
+	HTML  string `json:"html"`
+	Title string `json:"title"`
+}
+
+func SaveSite(site Site) error {
+
+	file, err := os.ReadFile("data/sites.json")
+	if err != nil {
+		return err
+	}
+
+	var sites []Site
+
+	json.Unmarshal(file, &sites)
+
+	sites = append(sites, site)
+
+	updated, _ := json.MarshalIndent(sites, "", "  ")
+
+	return os.WriteFile("data/sites.json", updated, 0644)
+}
