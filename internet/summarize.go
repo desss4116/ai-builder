a1a69dab32fb75fe64215467d5eaf3f41b90611e
desss4116 <aminjon01q@gmail.com>
2026-05-08 16:49:36 +0500

    Create summarize.go

---
+++ internet/summarize.go
@@ -0,0 +1,13 @@
+package internet
+
+func Summarize(content string) string {
+
+	return `
+На основе анализа информации:
+
+Главные выводы:
+- качество фильмов выросло
+- cinematic experience стал важнее
+- premium storytelling доминирует
+`
+}
