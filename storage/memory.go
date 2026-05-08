4f82b6cfc2ade5dd6b61921fda304a00dd190040
desss4116 <aminjon01q@gmail.com>
2026-05-08 15:16:12 +0500

    Update memory.go

--- storage/memory.go
+++ storage/memory.go
@@ -1,1 +1,29 @@
+package storage
 
+import (
+	"encoding/json"
+	"os"
+)
+
+type Memory struct {
+	UserID string `json:"user_id"`
+	Query  string `json:"query"`
+}
+
+func SaveMemory(memory Memory) error {
+
+	file, err := os.ReadFile("data/memory.json")
+	if err != nil {
+		return err
+	}
+
+	var memories []Memory
+
+	json.Unmarshal(file, &memories)
+
+	memories = append(memories, memory)
+
+	updated, _ := json.MarshalIndent(memories, "", "  ")
+
+	return os.WriteFile("data/memory.json", updated, 0644)
+}
