a07855d275f102d5dfbd316097653d13d9e06dac
desss4116 <aminjon01q@gmail.com>
2026-05-08 15:08:49 +0500

    Update intent.go

--- parser/intent.go
+++ parser/intent.go
@@ -1,1 +1,24 @@
+package parser
 
+import "strings"
+
+func IsWebsiteRequest(msg string) bool {
+	msg = strings.ToLower(msg)
+
+	keywords := []string{
+		"создай",
+		"site",
+		"website",
+		"landing",
+		"лендинг",
+		"сайт",
+	}
+
+	for _, k := range keywords {
+		if strings.Contains(msg, k) {
+			return true
+		}
+	}
+
+	return false
+}
