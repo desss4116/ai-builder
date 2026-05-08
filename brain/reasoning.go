c9ccba3fe8bacafae68253e5d638313e1a545b0a
desss4116 <aminjon01q@gmail.com>
2026-05-08 17:00:34 +0500

    Update reasoning.go

--- brain/reasoning.go
+++ brain/reasoning.go
@@ -16,67 +16,52 @@ func DetectIntent(query string) string {
 
 func Think(query string, data string) string {
 
+	knowledge := GetKnowledge(query)
+
 	q := strings.ToLower(query)
 
-	if strings.Contains(q, "фильм") {
+	/*
+	   TOP REQUESTS
+	*/
+
+	if strings.Contains(q, "топ") {
 
 		return `
-🎬 Топ фильмы:
+🔥 Лучшие результаты:
 
-1. Interstellar
-2. Oppenheimer
-3. Dune Part Two
-4. Blade Runner 2049
-5. Joker
-6. The Batman
-7. Avatar
-8. John Wick 4
-9. Napoleon
-10. The Creator
-`
+` + knowledge
 	}
 
-	if strings.Contains(q, "apple") {
-
-		return `
-🍎 Apple:
-
-Apple — одна из самых влиятельных
-технологических компаний мира.
-
-Главные особенности:
-- premium UI/UX
-- минимализм
-- экосистема
-- плавные анимации
-- high-end branding
-`
-	}
-
-	if strings.Contains(q, "netflix") {
-
-		return `
-🎬 Netflix UI Architecture:
-
-- cinematic layout
-- dark gradients
-- hover previews
-- recommendation engine
-- adaptive personalization
-- smooth transitions
-`
-	}
+	/*
+	   EDUCATION
+	*/
 
 	if strings.Contains(q, "что такое") {
 
 		return `
 📘 Объяснение:
 
-` + data
+` + knowledge
 	}
 
-	return `
-🌐 AI Analysis:
+	/*
+	   WORD MEANING
+	*/
 
-` + data
+	if strings.Contains(q, "значение") {
+
+		return `
+📖 Значение:
+
+` + knowledge
+	}
+
+	/*
+	   GENERAL
+	*/
+
+	return `
+🌐 Ответ:
+
+` + knowledge
 }
