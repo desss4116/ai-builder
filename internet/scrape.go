cdc523c81789146ee7747bc4c43e54555386dd33
desss4116 <aminjon01q@gmail.com>
2026-05-08 18:07:40 +0500

    Update scrape.go

--- internet/scrape.go
+++ internet/scrape.go
@@ -1,23 +1,71 @@
 package internet
 
 import (
+	"io"
+	"net/http"
 	"regexp"
 	"strings"
 )
 
-func ScrapeContent(html string) string {
+func ScrapePage(url string) string {
+
+	req, err := http.NewRequest(
+		"GET",
+		url,
+		nil,
+	)
+
+	if err != nil {
+		return ""
+	}
+
+	req.Header.Set(
+		"User-Agent",
+		"Mozilla/5.0",
+	)
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return ""
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return ""
+	}
+
+	html := string(body)
+
+	/*
+	   REMOVE TAGS
+	*/
 
 	re := regexp.MustCompile(`<[^>]*>`)
 
-	clean := re.ReplaceAllString(html, "")
+	text :=
+		re.ReplaceAllString(
+			html,
+			" ",
+		)
 
-	clean = strings.ReplaceAll(clean, "\n", " ")
+	text = strings.TrimSpace(text)
 
-	clean = strings.ReplaceAll(clean, "\t", " ")
+	/*
+	   CLEAN SPACES
+	*/
 
-	if len(clean) > 4000 {
-		clean = clean[:4000]
+	text = regexp.MustCompile(`\s+`).
+		ReplaceAllString(text, " ")
+
+	if len(text) > 3000 {
+		text = text[:3000]
 	}
 
-	return clean
+	return text
 }
