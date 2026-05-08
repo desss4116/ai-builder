ce91d38c1df6fda80a519dc63c60bc5dd2193fba
desss4116 <aminjon01q@gmail.com>
2026-05-08 15:36:10 +0500

    Delete knowledge directory

--- knowledge/knowledge.go
+++
@@ -1,9 +0,0 @@
-package knowledge
-
-var WebsiteKnowledge = []string{
-	"Premium websites use spacing.",
-	"Modern UI needs gradients.",
-	"Animations improve UX.",
-	"Large typography increases authority.",
-	"Minimalism feels premium.",
-}
