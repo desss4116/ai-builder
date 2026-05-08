82ea967c7e7dcd39be7ae62e0d639d27825881ec
desss4116 <aminjon01q@gmail.com>
2026-05-08 18:54:50 +0500

    Create crawl.go

---
+++ crawler/crawl.go
@@ -0,0 +1,67 @@
+package crawler
+
+import (
+	"io"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+)
+
+func Crawl(url string) string {
+
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return ""
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return ""
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return ""
+	}
+
+	html := string(body)
+
+	// remove scripts
+	reScript := regexp.MustCompile(`(?s)<script.*?</script>`)
+	html = reScript.ReplaceAllString(html, "")
+
+	// remove styles
+	reStyle := regexp.MustCompile(`(?s)<style.*?</style>`)
+	html = reStyle.ReplaceAllString(html, "")
+
+	// remove tags
+	reTags := regexp.MustCompile(`<[^>]+>`)
+	text := reTags.ReplaceAllString(html, " ")
+
+	// cleanup
+	text = strings.ReplaceAll(text, "\n", " ")
+	text = strings.ReplaceAll(text, "\r", " ")
+
+	reSpace := regexp.MustCompile(`\s+`)
+	text = reSpace.ReplaceAllString(text, " ")
+
+	text = strings.TrimSpace(text)
+
+	if len(text) > 4000 {
+		text = text[:4000]
+	}
+
+	return text
+}
