d9076106864764591f6ff62d280b9b4d9ad52b8b
desss4116 <aminjon01q@gmail.com>
2026-05-08 15:06:54 +0500

    Update scrape.go

--- internet/scrape.go
+++ internet/scrape.go
@@ -1,1 +1,21 @@
+package internet
 
+import (
+	"regexp"
+)
+
+func ExtractTitles(html string) []string {
+	re := regexp.MustCompile(`<a[^>]*class="result__a"[^>]*>(.*?)</a>`)
+
+	matches := re.FindAllStringSubmatch(html, -1)
+
+	var results []string
+
+	for _, match := range matches {
+		if len(match) > 1 {
+			results = append(results, match[1])
+		}
+	}
+
+	return results
+}
