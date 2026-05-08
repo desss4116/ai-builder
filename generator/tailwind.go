2f052448fbc7632c2b9961dbea3f69901587ae1f
desss4116 <aminjon01q@gmail.com>
2026-05-08 17:11:56 +0500

    Create tailwind.go

---
+++ generator/tailwind.go
@@ -0,0 +1,15 @@
+package generator
+
+func GenerateTailwindSystem() string {
+
+	return `
+Design System:
+
+- spacing scale
+- typography hierarchy
+- premium dark mode
+- luxury gradients
+- glassmorphism
+- cinematic layouts
+`
+}
