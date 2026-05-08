c40fcad0c96fda5edc6877d87c72b2c50d21bb51
desss4116 <aminjon01q@gmail.com>
2026-05-08 15:16:31 +0500

    Update knowledge.go

--- knowledge/knowledge.go
+++ knowledge/knowledge.go
@@ -1,1 +1,9 @@
+package knowledge
 
+var WebsiteKnowledge = []string{
+	"Premium websites use spacing.",
+	"Modern UI needs gradients.",
+	"Animations improve UX.",
+	"Large typography increases authority.",
+	"Minimalism feels premium.",
+}
