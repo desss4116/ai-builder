b75cf4b8ba1d8ee38cfeed49270353081da8bd5d
desss4116 <aminjon01q@gmail.com>
2026-05-08 15:09:17 +0500

    Update chat.go

--- parser/chat.go
+++ parser/chat.go
@@ -1,1 +1,25 @@
+package parser
 
+import (
+	"strings"
+)
+
+func BuildChatResponse(query string, results []string) string {
+	if len(results) == 0 {
+		return "Я не нашёл информацию."
+	}
+
+	var response strings.Builder
+
+	response.WriteString("🌐 Вот что я нашёл:\n\n")
+
+	for i, r := range results {
+		if i >= 5 {
+			break
+		}
+
+		response.WriteString("• " + r + "\n")
+	}
+
+	return response.String()
+}
