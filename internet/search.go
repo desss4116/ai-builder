b127c1ef3faddf39f8b9be0b618aefc24fe0e2b9
desss4116 <aminjon01q@gmail.com>
2026-05-08 15:06:26 +0500

    Update search.go

--- internet/search.go
+++ internet/search.go
@@ -1,1 +1,34 @@
+package internet
 
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
+
+func SearchInternet(query string) (string, error) {
+	searchURL := "https://html.duckduckgo.com/html/?q=" + url.QueryEscape(query)
+
+	req, err := http.NewRequest("GET", searchURL, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
